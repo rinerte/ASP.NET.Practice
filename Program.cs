8ee1871e218c8229696fea0c4c1f6dfc1b471353
rinerte <rinerte@gmail.com>
2025-06-23 16:09:41 +0900

    add logging in development

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.AspNetCore.HttpLogging;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpLogging(opts =>
+    opts.LoggingFields = HttpLoggingFields.RequestProperties);
+
+builder.Logging.AddFilter(
+    "Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpLogging();
+}
 app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/person", () => new Person("Pavel", "Dudarev"));
 app.Run();
+
+
+public record Person(string FirstName, string LastName);
